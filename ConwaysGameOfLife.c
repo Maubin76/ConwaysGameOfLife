d066b2aeb81ba4ef7afed7a5bedf213f62da61dc
Mathieu Aubin <maubin@enssat.fr>
2024-04-21 19:25:27 +0200

    update the board generation

--- ConwaysGameOfLife.c
+++ ConwaysGameOfLife.c
@@ -5,8 +5,19 @@ void main() {
     char **board = createBoard();
     // Randomize board with ALIVE_CHANCE chance of a cell being alive
     randomizeBoard(board);
-    printBoard(board);
-
+    sleep(GENERATION_RATE);
+    while(1){
+        // Clear screen
+        system("clear");
+        // Print board
+        printBoard(board);
+        // Wait 1s
+        sleep(GENERATION_RATE);
+        // Calculate next generation
+        nextGeneration(board);
+        // print the board
+        printBoard(board);
+    }
 }
 
 char** createBoard(){
@@ -37,4 +48,49 @@ void randomizeBoard(char **board){
             }
         }
     }
+}
+
+void nextGeneration(char **board){
+    char **newBoard = createBoard();
+    for(int i = 0; i < BOARD_HEIGHT; i++){
+        for(int j = 0; j < BOARD_WIDTH; j++){
+            int neighbors = countNeighbors(board, i, j);
+            if(board[i][j] == ALIVE){
+                if(neighbors < 2 || neighbors > 3){
+                    newBoard[i][j] = DEAD;
+                } else {
+                    newBoard[i][j] = ALIVE;
+                }
+            } else {
+                if(neighbors == 3){
+                    newBoard[i][j] = ALIVE;
+                } else {
+                    newBoard[i][j] = DEAD;
+                }
+            }
+        }
+    }
+    for(int i = 0; i < BOARD_HEIGHT; i++){
+        for(int j = 0; j < BOARD_WIDTH; j++){
+            board[i][j] = newBoard[i][j];
+        }
+    }
+}
+
+int countNeighbors(char **board, int x, int y){
+    int count = 0;
+    for(int i = -1; i <= 1; i++){
+        for(int j = -1; j <= 1; j++){
+            if(i == 0 && j == 0){
+                continue;
+            }
+            if(x + i < 0 || x + i >= BOARD_HEIGHT || y + j < 0 || y + j >= BOARD_WIDTH){
+                continue;
+            }
+            if(board[x + i][y + j] == ALIVE){
+                count++;
+            }
+        }
+    }
+    return count;
 }
--- ConwaysGameOfLife.h
+++ ConwaysGameOfLife.h
@@ -1,17 +1,22 @@
 #define CONWAYSGAMEOFLIFE_H
 #ifdef CONWAYSGAMEOFLIFE_H
 
-#define BOARD_WIDTH 155
-#define BOARD_HEIGHT 45
+// These dimensions depend on the terminal size, they are adapted to mine
+#define BOARD_WIDTH 157
+#define BOARD_HEIGHT 47
 #define ALIVE_CHANCE 0.1
+#define GENERATION_RATE 1
 #define ALIVE 'O'
 #define DEAD '.'
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void printBoard(char **);
 char** createBoard();
 void randomizeBoard(char **);
+void nextGeneration(char **);
+int countNeighbors(char **, int, int);
 
 #endif
